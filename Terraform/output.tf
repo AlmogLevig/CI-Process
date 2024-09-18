91beed65f07d7f310080bda0fcbe3b9d8d4c6ca2
almog.levi <almog.levig@gmail.com>
2024-09-18 15:53:29 +0300

    Updates

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,7 +1,7 @@
-output "jenkins_agent_private_ip" {
+output "test_ec2_private_ip" {
   value = aws_instance.test_ec2.private_ip
 }
-output "jenkins_agent_public_ip" {
+output "test_ec2_public_ip" {
   value = aws_instance.test_ec2.public_ip
 }
 output "url" {
